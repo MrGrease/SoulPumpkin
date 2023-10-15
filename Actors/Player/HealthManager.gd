b807d4277a32f790c3464a4076de9ca23a4dfb3f
unknown <mehmetemreyorulmaz@gmail.com>
2023-10-16 00:37:53 +0300

    Added health manager and a basic leaf monster scene

---
+++ Actors/Player/HealthManager.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+var maxHealth = 100
+var currentHealth = 100
+
+signal signalDead
+signal signalHurt
+signal signalHeal
+
+func _ready():
+	init()
+
+func init():
+	currentHealth=maxHealth
+
+func applyDamage(damage):
+	if currentHealth <=0:
+		return
+	else:
+		currentHealth-=damage
+	if currentHealth <=0:
+		emit_signal("signalDead")
+	emit_signal("signalHurt")
+
+func heal(healing):
+	if currentHealth <=0:
+		return
+	currentHealth += healing
+	emit_signal("signalHeal")
+
--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -3,11 +3,15 @@ extends CharacterBody3D
 @onready var movementManager = $MovementManager
 @onready var camera = $Camera
 @onready var graphics = $Graphics
+@onready var healthManager = $HealthManager
 var mouseSensitivity =0.25
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	movementManager.setBody(self)
+	
+	healthManager.init()
+	healthManager.connect("signalDead",die)
 
 func _process(_delta):
 	var move_vec = Vector3()
@@ -37,3 +41,12 @@ func _input(event):
 
 func rotateMesh():
 	graphics.rotation_degrees.y = camera.rotation_degrees.y 
+
+func die():
+	print("dead!")
+
+func hurt(damage):
+	healthManager.hurt(damage)
+
+func heal(amount):
+	healthManager.heal(amount)
